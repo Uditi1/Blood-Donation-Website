3a3a48247539477817590c536c0750136714cd16
Hemant <Srivastavhemant17@gmail.com>
2023-05-18 17:02:23 +0530

    update component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Main from "./Images/Main.jpg";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -17,12 +18,12 @@ const Hero = () => {
           <span className="text-red-600">saves lives</span> and enhances social
           solidarity.
         </p>
-        <button className="btn-1 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4 mr-4">
+        <Link  to="/LookingBlood" className="btn-1 bg-red-600 hover:bg-red-700 text-lg text-white font-bold py-3 px-4 rounded mb-4 mr-4">
           Looking for Blood
-        </button>
-        <button className="btn-2 bg-white hover:bg-red-600 text-white-600 font-bold py-2 px-4 border border-red-600 rounded mb-4">
+        </Link>
+        <Link to="/DonorList" className="btn-2 bg-white hover:bg-red-600 text-white-600 font-bold py-3 px-4 border border-red-600 rounded mb-4">
           Donate Blood
-        </button>
+        </Link>
       </div>
       <div className="right-img">
         <img
@@ -36,34 +37,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-// import React from "react";
-// import Main from "./Images/Main.jpg"; // Replace with your own hero image
-
-// function HeroSection() {
-//   return (
-//     <div className="flex flex-col lg:flex-row items-center justify-between px-4 py-16 lg:py-24 bg-gray-100">
-//       <div className="max-w-lg">
-//         <h1 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-6">
-//           Donate Blood, Save Lives
-//         </h1>
-//         <p className="text-gray-700 text-lg mb-8">
-//           Every day, blood transfusions help save the lives of people in need.
-//           By donating blood, you can make a difference and help save lives.
-//         </p>
-//         <button className="bg-red-500 text-white py-2 px-4 rounded-full hover:bg-red-600 transition-colors duration-300">
-//           Donate Now
-//         </button>
-//       </div>
-//       <div className="mt-12 lg:mt-0">
-//         <img
-//           src={Main}
-//           alt="Donate Blood, Save Lives"
-//           className="mix-blend-darken max-w-full absolute top-20 right-0 overflow-hidden"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HeroSection;
