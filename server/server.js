6d480f68686a43cf8962bee0a3feb97928d0ed33
Uditi1 <udititiwari@gmail.com>
2023-05-30 17:40:17 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
+const cors = require('cors')
 
 connectDB();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.get("/", (req, res, next) => {
   res.send("Api running");
